329cc4e66e72f7905837967b5699f37500c1e936
MohitSnehal <mohitsnehal93@gmail.com>
2023-03-11 17:05:30 -0700

    made changes

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,3 +1,25 @@
+def process_last_column(data):
+    shortened_labels = ['PartOfAHorizontalPortScan','Okiru','Benign','DDoS','C&C','Attack','C&C-HeartBeat','C&C-FileDownload','C&C-Torii','C&C-HeartBeat-FileDownload','FileDownload','C&C-Mirai','Okiru-Attack']
+    for short_label in shortened_labels:
+        if short_label.casefold() in data[len(data)-1].casefold():
+            data[len(data)-1] = short_label
+            break
+    return data
+
+def convert_hyphens_to_zero(data, fields):
+    column_no = 0
+    numerical_columns = {'duration','orig_bytes','resp_bytes'}
+    try:
+        for column in data:
+            if column == "-" and fields[column_no] in numerical_columns:
+                data[column_no] = 0
+            column_no += 1
+        return data
+    except Exception as e:
+        print("-----------xxxx----------\n")
+        print("exception: " + str(e))
+        print("-----------xxxx----------\n")
+
 def data_cleanup():
     # All files  in the path
     import pathlib
@@ -46,6 +68,7 @@ def data_cleanup():
                         if not header_written:
                             with open(csv_file, 'a') as csvfile:
                                 writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
+                                final_file_fields[len(final_file_fields)-1] = "label"
                                 writer.writerow(final_file_fields)
                                 header_written = True
 
@@ -56,11 +79,14 @@ def data_cleanup():
                             for column_no in column_no_to_remove:
                                 del data[column_no]
                             # print("final row to be written: " + str(data))
+                            data = process_last_column(data)
+                            data = convert_hyphens_to_zero(data, final_file_fields)
                             with open(csv_file, 'a') as csvfile:
                                 writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
                                 writer.writerow(data)
                     except Exception as e:
-                        print("-----------xxxx----------" + str(data_length) + "\n" + str(row_count) + "\n" + str(data))
+                        print("EXCEPTION")
+                        print("-----------xxxx----------\n" + str(data_length) + "\n" + str(row_count) + "\n" + str(data))
                         print(str(e))
                         print("-----------xxxx----------\n")
                 row_count += 1
